e4eb889db217e0fd068f6b113edcb9d2750ffde6
Dave Sommerville <dave.r.sommerville@outlook.com>
2024-12-04 13:32:15 -0600

    Cookie setters and getters created

--- src/js/app.js
+++ src/js/app.js
@@ -5,93 +5,97 @@
 <-----------------------------------*/
 
 function select(selector, scope = document) {
-	return scope.querySelector(selector);
+    return scope.querySelector(selector);
 }
 
 function listen(event, selector, callback) {
-	return selector.addEventListener(event, callback);
+    return selector.addEventListener(event, callback);
 }
+
 const { log } = console;
-const { cookie } = document;
-
-// Setting expiry 
-const date = new Date();
-date.setSeconds(date.getSeconds() + 15);
-let UTCDate = date.toUTCString();
-
-let maxAge = 15;
-// Below is mostly for reference atm
-document.cookie = `City=Winnipeg; path=/; max-age=${maxAge}; SameSite=Lax`;
-let fullNameKey = encodeURIComponent('Full name');
-let fullNameValue = encodeURIComponent('John Smith');
-let cityName = encodeURIComponent('New York');
-document.cookie = `${fullNameKey}=${fullNameValue}`;
-document.cookie = `City=${cityName}`;
-
-// This will perform a log function instead
-// Must remember to encode 
-function printCookies() {
-	if (document.cookie.length > 0) {
-		const cookies = document.cookie.split('; ');
-		for (let i = 0; i < cookies.length; i++) {
-			console.log (`
-				${decodeURIComponent(cookies[i].split('=')[0])}: ${decodeURIComponent(cookies[i].split('=')[1])}
-			`);
-		}
-	} else {
-	console.log('No coookies found');
-	}
-}
-printCookies();
-/*------------------------------------>
-    Variable Declarations
-<-----------------------------------*/
-const toggle = select("#toggle");
 
 const userAgent = navigator.userAgent;
-let userLang = navigator.language;
 let pageWidth = window.innerWidth;
 let pageHeight = window.innerHeight;
 
+const date = new Date();
+date.setSeconds(date.getSeconds() + 15); 
+let UTCDate = date.toUTCString();
+
 /*------------------------------------>
-    Utility Functions 
+    Cookie Functions
 <-----------------------------------*/
-function getOperatingSystem() {
-	if (userAgent.includes("Windows")) {
-			opSy.innerText = "Windows"; /* Redirecting output to cookie */
-	} else if (userAgent.includes("Macintosh")) {
-			opSy.innerText = "Mac OS";
-	} else if (userAgent.includes("iPhone") || userAgent.includes("iPad") || userAgent.includes("iPod")) {
-			opSy.innerText = "iOS";
-	} else if (userAgent.includes("Android")) {
-			opSy.innerText = "Android";
-	} else if (userAgent.includes("Linux")) {
-			opSy.innerText = "Linux";
-	} else {
-			opSy.innerText = "Your Operating System isn't recognized.";
-	}
+
+function setCookie(name, value) {								// Catch this 
+    document.cookie = `${name}=${encodeURIComponent(value)};expires=${UTCDate};path=/;SameSite=Lax`;
 }
+
+function getCookie(name) {
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const cookies = decodedCookie.split(';');
+    name = name + '=';
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        if (cookie.indexOf(name) === 0) {
+            return cookie.substring(name.length, cookie.length);
+        }
+    }
+    return null; // It wasn't null, it was something else 
+}
+
+let operatingSystem = 'Unknown OS';
+
+function getOperatingSystem() {
+    if (userAgent.includes('Windows')) {
+        operatingSystem = 'Operating System: Windows';
+    } else if (userAgent.includes('Macintosh')) {
+        operatingSystem = 'Operating System: Mac OS';
+    } else if (userAgent.includes('iPhone') || userAgent.includes('iPad') || userAgent.includes('iPod')) {
+        operatingSystem = 'Operating System: iOS';
+    } else if (userAgent.includes('Android')) {
+        operatingSystem = 'Operating System: Android';
+    } else if (userAgent.includes('Linux')) {
+        operatingSystem = 'Operating System: Linux';
+    }
+}
+
+let browser = 'Unknown Browser';
 
 function getBrowser() {
-	if (userAgent.includes("Firefox")) {
-			browser.innerText = "Firefox";
-	} else if (userAgent.includes("Edge") || userAgent.includes("edg")) {
-			browser.innerText = "Edge";
-	} else if (userAgent.includes("Chrome") && !userAgent.includes("Chromium")) {
-			browser.innerText = "Chrome";
-	} else if (userAgent.includes("Opera") || userAgent.includes("OPR")) {
-			browser.innerText = "Opera";
-	} else if (userAgent.includes("Safari") && !userAgent.includes("Chrome")) {
-			browser.innerText = "Safari";
-	} else {
-			browser.innerText = "Your browser isn't recognized.";
-	}
+    if (userAgent.includes('Firefox')) {
+        browser = 'Browser: Firefox';
+    } else if (userAgent.includes('Edge') || userAgent.includes('edg')) {
+        browser = 'Browser: Edge';
+    } else if (userAgent.includes('Chrome') && !userAgent.includes('Chromium')) {
+        browser = 'Browser: Chrome';
+    } else if (userAgent.includes('Opera') || userAgent.includes('OPR')) {
+        browser = 'Browser: Opera';
+    } else if (userAgent.includes('Safari') && !userAgent.includes('Chrome')) {
+        browser = 'Browser: Safari';
+    }
 }
 
-listen("change", toggle, () => {
-	// if(toggle.checked) {
-	// 	targetShape.classList.add('circle');
-	// } else {
-	// 	targetShape.classList.remove('circle');
-	// }
-});
+getOperatingSystem();
+getBrowser();
+setCookie('Operating System', operatingSystem);
+setCookie('Browser', browser);
+setCookie('Screen Width', pageWidth);
+setCookie('Screen Height', pageHeight);
+
+function displayCookies() {
+    const os = getCookie('Operating System');
+    const browser = getCookie('Browser');
+    const screenWidth = getCookie('Screen Width');
+    const screenHeight = getCookie('Screen Height');
+
+    log(os);
+    log(browser);
+    log('Screen Width:', screenWidth); // Haven't been able to remove this portion
+    log('Screen Height:', screenHeight);
+}
+
+
+displayCookies();
+log('Operating System (variable):', operatingSystem);
+log('UTC Expiration Date:', UTCDate);
+log('All Cookies:', document.cookie);
